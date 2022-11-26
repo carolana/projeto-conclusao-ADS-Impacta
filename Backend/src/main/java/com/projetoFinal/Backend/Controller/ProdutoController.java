865524761df37f3efd0c0cf521dc407ae3434cff
Arturo Americo <arturoamerico@tokenlab.com.br>
2022-11-26 20:21:26 -0300

    correção de cors

--- Backend/src/main/java/com/projetoFinal/Backend/Controller/ProdutoController.java
+++ Backend/src/main/java/com/projetoFinal/Backend/Controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.projetoFinal.Backend.Model.Produto;
 import com.projetoFinal.Backend.Repository.ProdutoRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
