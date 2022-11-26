c1a273d8d90c32b0d92c8e5a765fb9841658467a
Arturo Americo <arturoamerico@tokenlab.com.br>
2022-11-26 11:46:34 -0300

    pequenos ajustes no front e no back

--- front-end-conclusao-ADS-Impacta/src/app/modelo/produtos.service.ts
+++ front-end-conclusao-ADS-Impacta/src/app/modelo/produtos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,27 +7,36 @@ export class ProdutosService {
   private URL: string;
 
   constructor(private http: HttpClient) {
-    //TO-DO: COLOCAR URL QUANDO DISPONIBILIZADA E VERIFICAR MEOTÓDOS
-    this.URL = ''; //Link Base do BackEnd
+    this.URL = 'http://localhost:8080'; //URL Base do BackEnd
   }
 
   obterProdutos(): Observable<any> {
-    return this.http.get<any>(`${this.URL}/produtos`); //Link para Obter todos os Produtos. Método GET
+    return this.http.get<any>(`${this.URL}/produtos`); //URL para Obter todos os Produtos. Método GET
   }
 
   pesquisarProdutoId(id: any): Observable<any> {
-    return this.http.get<any>(`${this.URL}/produtos/${id}`); //Link para "Pegar" o Produto pelo ID. Método GET
+    return this.http.get<any>(`${this.URL}/produtos/${id}`); //URL para "Pegar" o Produto pelo ID. Método GET
   }
 
   atualizarProdutoId(prod: any): Observable<any> {
-    return this.http.put<any>(`${this.URL}/produtos`, prod); //Link para Atualizar um Produto no qual é passado um Produto com as novas informações. Método PUT (Objeto TODO) é diferente do Método PATCH (PARTE do Objeto).
+    return this.http.put<any>(`${this.URL}/produtos`, prod); //URL para Atualizar um Produto no qual é passado um Produto com as novas informações. Método PUT (Objeto TODO) é diferente do Método PATCH (PARTE do Objeto).
   }
 
   adicionaProduto(prod: any): Observable<any> {
-    return this.http.post<any>(`${this.URL}/produtos`, prod); //Link para Adicionar um Novo Produto. Método POST
+    return this.http.post<any>(`${this.URL}/produtos`, prod); //URL para Adicionar um Novo Produto. Método POST
   }
 
   excluiProdutoId(id: any): Observable<any> {
-    return this.http.delete<any>(`${this.URL}/produtos/${id}`); //Link para Deletar um Produto pelo ID. Método DELETE
+
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      body: {
+        id: id
+      },
+    };
+
+    return this.http.delete<any>(`${this.URL}/produtos`, options); //URL para Deletar um Produto pelo ID. Método DELETE
   }
 }
